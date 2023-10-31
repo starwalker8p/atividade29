07c387d18af6466411608a73d95eb43c47853ccf
starwalker8p <starwalker8p@gmail.com>
2023-10-31 16:32:52 -0300

    fiz

---
+++ peso.py
@@ -0,0 +1,4 @@
+n1,n2,n3,n4,n5=float(input("peso 1: ")),float(input("peso 2: ")),float(input("peso 3: ")),float(input("peso 4: ")),float(input("peso 5: "))
+pesos=[n1,n2,n3,n4,n5]
+print(f"maior: {max(pesos)}kg")
+print(f"menor: {min(pesos)}kg")
